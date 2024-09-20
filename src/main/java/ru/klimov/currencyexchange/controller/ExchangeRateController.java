25a5de4951d4bea28f87e917d37647ee999853b7
Maksim Klimov <maksimklimov779@gmail.com>
2024-09-20 14:39:42 +0300

    fix: remove commented code

--- src/main/java/ru/klimov/currencyexchange/controller/ExchangeRateController.java
+++ src/main/java/ru/klimov/currencyexchange/controller/ExchangeRateController.java
@@ -25,42 +25,17 @@ public class ExchangeRateController {
 
     @GetMapping("/{codePair}")
     public ExchangeRate getExchangeRate(@PathVariable String codePair) {
-//        Optional<ExchangeRate> result = exchangeRateService.findExchangeRateByCodePair(
-//                codePair.substring(0, 3),
-//                codePair.substring(3));
-//        if (result.isPresent()) {
-//            return new ResponseEntity<>(result.get(), HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-//        }
         return exchangeRateService.getExchangeRateByCodePair(codePair);
     }
 
     @PostMapping(consumes = "application/x-www-form-urlencoded")
     public ExchangeRate createExchangeRate(@RequestParam Map<String, String> exchangeRateParamMap) {
-//        Currency baseCurrency = currencyService.findByCurrencyCode(multiValueMap.get("baseCurrencyCode")).get();
-//        Currency targetCurrency = currencyService.findByCurrencyCode(multiValueMap.get("targetCurrencyCode")).get();
-//        ExchangeRate exchangeRate = new ExchangeRate();
-//        exchangeRate.setBaseCurrency(baseCurrency);
-//        exchangeRate.setTargetCurrency(targetCurrency);
-//        exchangeRate.setRate(new BigDecimal(multiValueMap.get("rate")));
-//        exchangeRateService.save(exchangeRate);
         return exchangeRateService.createExchangeRate(exchangeRateParamMap);
     }
 
     @PatchMapping(path = "/{codePair}", consumes = "application/x-www-form-urlencoded")
     public ExchangeRate updateExchangeRate(@PathVariable String codePair,
                                            @RequestParam Map<String, String> rateParamMap) {
-//        Optional<ExchangeRate> result = exchangeRateService.findExchangeRateByCodePair(
-//                codePair.substring(0, 3),
-//                codePair.substring(3));
-//        if (result.isPresent()) {
-//            result.get().setRate(new BigDecimal(multiValueMap.get("rate")));
-//            exchangeRateService.update(result.get());
-//            return new ResponseEntity<>(result.get(), HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-//        }
         return exchangeRateService.updateExchangeRate(codePair, rateParamMap);
     }
 }
