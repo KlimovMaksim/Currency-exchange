59153ccb01168d87eebe6c7a2e1daf64218fd41a
Maksim Klimov <maksimklimov779@gmail.com>
2024-09-20 17:41:15 +0300

    feat: add global exception handler

--- src/main/java/ru/klimov/currencyexchange/controller/CurrencyController.java
+++ src/main/java/ru/klimov/currencyexchange/controller/CurrencyController.java
@@ -2,14 +2,8 @@ package ru.klimov.currencyexchange.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.web.bind.annotation.*;
 import ru.klimov.currencyexchange.entity.Currency;
-import ru.klimov.currencyexchange.exceptions.CurrencyAlreadyExistException;
-import ru.klimov.currencyexchange.exceptions.CurrencyNotFoundException;
-import ru.klimov.currencyexchange.exceptions.InvalidCurrencyDataException;
 import ru.klimov.currencyexchange.service.CurrencyService;
 
 import java.util.Map;
@@ -39,29 +33,5 @@ public class CurrencyController {
     @ResponseStatus(HttpStatus.CREATED)
     public Currency createCurrency(@RequestParam Map<String, String> currencyParamMap) {
         return currencyService.createCurrency(currencyParamMap);
-    }
-
-    @ExceptionHandler(CurrencyNotFoundException.class)
-    public ResponseEntity<ProblemDetail> handleCurrencyNotFoundException(CurrencyNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage()));
-    }
-
-    @ExceptionHandler(InvalidCurrencyDataException.class)
-    public ResponseEntity<ProblemDetail> handleInvalidCurrencyDataException(InvalidCurrencyDataException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage()));
-    }
-
-    @ExceptionHandler(CurrencyAlreadyExistException.class)
-    public ResponseEntity<ProblemDetail> handleCurrencyAlreadyExistException(CurrencyAlreadyExistException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage()));
-    }
-
-    @ExceptionHandler(CannotGetJdbcConnectionException.class)
-    public ResponseEntity<ProblemDetail> handleCannotGetJdbcConnectionException(CannotGetJdbcConnectionException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Database connection error"));
     }
 }
---
+++ src/main/java/ru/klimov/currencyexchange/controller/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package ru.klimov.currencyexchange.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.klimov.currencyexchange.exceptions.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private ResponseEntity<ProblemDetail> createProblemDetail(HttpStatus status, String detail) {
+        return ResponseEntity.status(status).body(ProblemDetail.forStatusAndDetail(status, detail));
+    }
+
+    @ExceptionHandler(CurrencyAlreadyExistException.class)
+    public ResponseEntity<ProblemDetail> handleCurrencyAlreadyExistException(CurrencyAlreadyExistException ex) {
+        return createProblemDetail(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(CurrencyNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleCurrencyNotFoundException(CurrencyNotFoundException ex) {
+        return createProblemDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidCurrencyDataException.class)
+    public ResponseEntity<ProblemDetail> handleInvalidCurrencyDataException(InvalidCurrencyDataException ex) {
+        return createProblemDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(ExchangeRateAlreadyExistsException.class)
+    public ResponseEntity<ProblemDetail> handleExchangeRateAlreadyExistsException(ExchangeRateAlreadyExistsException ex) {
+        return createProblemDetail(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(ExchangeRateNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleExchangeRateNotFoundException(ExchangeRateNotFoundException ex) {
+        return createProblemDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidExchangeRateDataException.class)
+    public ResponseEntity<ProblemDetail> handleInvalidExchangeRateDataException(InvalidExchangeRateDataException ex) {
+        return createProblemDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(CannotGetJdbcConnectionException.class)
+    public ResponseEntity<ProblemDetail> handleCannotGetJdbcConnectionException(CannotGetJdbcConnectionException ex) {
+        return createProblemDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Database connection error");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetail> handleGenericException(Exception ex) {
+        return createProblemDetail(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred");
+    }
+}
