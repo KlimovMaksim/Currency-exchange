29ce090173dff177a813a16debce67c6ac1ef7f9
Maksim Klimov <maksimklimov779@gmail.com>
2024-10-08 11:45:12 +0300

    fix: missing database in containerÐ

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -14,19 +14,3 @@ create table currency_exchange_db.ExchangeRates (
      Rate numeric(12, 6) not null,
      unique(BaseCurrencyId, TargetCurrencyId)
 );
-
-/*insert into currency_exchange_db.Currencies (code, fullname, sign) values
-      ('USD', 'United States Dollar', '$'),
-      ('EUR', 'Euro', '€'),
-      ('GBP', 'British Pound', '£'),
-      ('JPY', 'Japanese Yen', '¥'),
-      ('AUD', 'Australian Dollar', 'A$');
-
-insert into currency_exchange_db.ExchangeRates (BaseCurrencyId, TargetCurrencyId, Rate) values
-       (1, 2, 0.85),  -- USD -> EUR
-       (1, 3, 0.75),  -- USD -> GBP
-       (1, 4, 110.15), -- USD -> JPY
-       (2, 1, 1.18),  -- EUR -> USD
-       (3, 1, 1.33),  -- GBP -> USD
-       (4, 1, 0.0091), -- JPY -> USD
-       (5, 1, 0.73);  -- AUD -> USD*/
