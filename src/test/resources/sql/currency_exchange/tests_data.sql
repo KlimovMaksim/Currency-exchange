20e38955be2daa8635bd829374ab490ec5cd18b3
Maksim Klimov <maksimklimov779@gmail.com>
2024-10-08 11:53:10 +0300

    fix: test data sql exception in tests

--- src/test/resources/sql/currency_exchange/tests_data.sql
+++ src/test/resources/sql/currency_exchange/tests_data.sql
@@ -1,15 +1,49 @@
 insert into currency_exchange_db.Currencies (code, fullname, sign) values
-   ('USD', 'United States Dollar', '$'),
-   ('EUR', 'Euro', '€'),
-   ('GBP', 'British Pound', '£'),
-   ('JPY', 'Japanese Yen', '¥'),
-   ('AUD', 'Australian Dollar', 'A$');
+    ('USD', 'United States Dollar', '$'),
+    ('EUR', 'Euro', '€'),
+    ('GBP', 'British Pound', '£'),
+    ('JPY', 'Japanese Yen', '¥'),
+    ('AUD', 'Australian Dollar', 'A$');
 
 insert into currency_exchange_db.ExchangeRates (BaseCurrencyId, TargetCurrencyId, Rate) values
-    (1, 2, 0.85),  -- USD -> EUR
-    (1, 3, 0.75),  -- USD -> GBP
-    (1, 4, 110.15), -- USD -> JPY
-    (2, 1, 1.18),  -- EUR -> USD
-    (3, 1, 1.33),  -- GBP -> USD
-    (4, 1, 0.0091), -- JPY -> USD
-    (5, 1, 0.73);  -- AUD -> USD
+    (
+        (select id from currency_exchange_db.Currencies where code = 'USD'),
+        (select id from currency_exchange_db.Currencies where code = 'EUR'),
+        0.85
+    ),  -- USD -> EUR
+
+    (
+        (select id from currency_exchange_db.Currencies where code = 'USD'),
+        (select id from currency_exchange_db.Currencies where code = 'GBP'),
+        0.75
+    ),  -- USD -> GBP
+
+    (
+        (select id from currency_exchange_db.Currencies where code = 'USD'),
+        (select id from currency_exchange_db.Currencies where code = 'JPY'),
+        110.15
+    ), -- USD -> JPY
+
+    (
+        (select id from currency_exchange_db.Currencies where code = 'EUR'),
+        (select id from currency_exchange_db.Currencies where code = 'USD'),
+        1.18
+    ),  -- EUR -> USD
+
+    (
+        (select id from currency_exchange_db.Currencies where code = 'GBP'),
+        (select id from currency_exchange_db.Currencies where code = 'USD'),
+        1.33
+    ),  -- GBP -> USD
+
+    (
+        (select id from currency_exchange_db.Currencies where code = 'JPY'),
+        (select id from currency_exchange_db.Currencies where code = 'USD'),
+        0.0091
+    ), -- JPY -> USD
+
+    (
+        (select id from currency_exchange_db.Currencies where code = 'AUD'),
+        (select id from currency_exchange_db.Currencies where code = 'USD'),
+        0.73
+    );  -- AUD -> USD
